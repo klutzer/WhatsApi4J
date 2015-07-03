54e2e0a07142b1c7685cc328a665b0904a51619e
klutzer <erico.klutzer@gmail.com>
2015-07-03 10:00:26 -0300

    Register new phone number ok

--- src/main/java/net/sumppen/whatsapi4j/WhatsApi.java
+++ src/main/java/net/sumppen/whatsapi4j/WhatsApi.java
@@ -47,8 +47,8 @@ public class WhatsApi {
     private final String WHATSAPP_REQUEST_HOST = "v.whatsapp.net/v2/code";      // The request code host.
     public static final String WHATSAPP_SERVER = "s.whatsapp.net";               // The hostname used to login/send messages.
     private final String WHATSAPP_DEVICE = "S40";                      // The device name.
-    private final String WHATSAPP_VER = "2.12.81";                // The WhatsApp version.
-    private final String WHATSAPP_USER_AGENT = "WhatsApp/2.12.81 S40Version/14.26 Device/Nokia302";// User agent used in request/registration code.
+    private final String WHATSAPP_VER = "2.12.82";                // The WhatsApp version.
+    private final String WHATSAPP_USER_AGENT = "WhatsApp/2.12.82 S40Version/14.26 Device/Nokia302";// User agent used in request/registration code.
     private final String WHATSAPP_VER_CHECKER = "https://coderus.openrepos.net/whitesoft/whatsapp_version"; // Check WhatsApp version
 
     private final Logger log = LoggerFactory.getLogger(WhatsApi.class);
